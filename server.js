4252e1713687eca70b8061aa9ca0b442f67c0598
Chris <chris284891@gmail.com>
2024-11-09 15:08:03 -0500

    Added delete row form, clear all, and styling

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // index.js
 const express = require("express");
 const { Pool } = require("pg");
+const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -11,6 +12,8 @@ const pool = new Pool({
     "postgres://udaendc20npfup:pe119b7c2a6aba549de38a2d3d07bc7c9340ed7f94166ff0316512b194883a611@c3cj4hehegopde.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/df3d3ojlucpms8", // Add Heroku's DATABASE_URL here
   ssl: { rejectUnauthorized: false }, // For Heroku SSL
 });
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.set("view engine", "ejs");
 
@@ -37,16 +40,48 @@ app.post("/add", async (req, res) => {
   try {
     // Insert the new row into the table
     const result = await pool.query(
-      'INSERT INTO pills (name, amount) VALUES ($1, $2)',
+      "INSERT INTO pills (name, amount) VALUES ($1, $2)",
       [name, amount]
     );
 
-    console.log("New row added:", result.rows[0]);
+    // console.log("New row added:", result.rows[0]);
 
     // Redirect back to the table view after inserting the row
-    res.redirect("/");
+    res.redirect('/');
   } catch (error) {
     console.error("Error inserting data into the database", error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+app.post("/delete", async (req, res) => {
+  const { name } = req.body;
+
+  try {
+    // Insert the new row into the table
+    const result = await pool.query("delete from pills where name = $1", [
+      name,
+    ]);
+
+    // console.log("Row deleted:", result.rows[0]);
+
+    // Redirect back to the table view after inserting the row
+    res.redirect('/');
+  } catch (error) {
+    console.error("Error deleting data", error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+app.post("/clear-all", async (req, res) => {
+  try {
+    // Perform the DELETE query to clear all rows from the people table
+    const result = await pool.query("DELETE FROM pills");
+
+    // res.render('result', { message: 'All records have been deleted.', success: true });
+    res.redirect('/');
+  } catch (error) {
+    console.error("Error executing query", error.stack);
     res.status(500).send("Internal Server Error");
   }
 });
